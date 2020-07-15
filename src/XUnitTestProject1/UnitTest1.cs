22f1e1a8233708a11c065d81a978159618b7cc22
Kay McCormick <kay@kaymccormick.com>
2020-07-15 16:11:09 -0700

    rework

--- src/XUnitTestProject1/UnitTest1.cs
+++ src/XUnitTestProject1/UnitTest1.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Threading;
+using System.Windows.Xps.Packaging;
+using Microsoft.VisualStudio.Threading;
 using RoslynCodeControls;
 using Xunit;
 
@@ -10,45 +17,84 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [WpfFact]
+        public void Test3()
+        {
+            ManualResetEvent mevent = new ManualResetEvent(false);
+            var startSecondaryThread = RoslynCodeControl.StartSecondaryThread(mevent, (d) =>
+            {
+
+            });
+
+            var joinableTaskFactory = new JoinableTaskFactory(new JoinableTaskContext());
+            joinableTaskFactory.Run(() => { return X1(mevent, startSecondaryThread, joinableTaskFactory); });
+
+
+        }
+
+        private async Task X1(ManualResetEvent mevent, Thread startSecondaryThread, JoinableTaskFactory joinableTaskFactory)
+        {
+            await mevent.ToTask();
+            await NewMethod1(startSecondaryThread, joinableTaskFactory);
+
+        }
+
+        private static async Task NewMethod1(Thread startSecondaryThread, JoinableTaskFactory jtf)
+        {
+            var d = Dispatcher.FromThread(startSecondaryThread);
+
+            var jtf2 = new JoinableTaskFactory(new JoinableTaskContext(RoslynCodeControl.SecondaryThread,
+                new DispatcherSynchronizationContext(d))); 
+            var b = await NewMethod(jtf, jtf2);
+            await jtf.SwitchToMainThreadAsync();
+            var bDocumentPaginator = b.DocumentPaginator;
+            // var dg = new PrintDialog();
+            // dg.PrintDocument(bDocumentPaginator, "");
+            var cTempDoc12Xps = @"c:\temp\doc12.xps";
+            File.Delete(cTempDoc12Xps);
+            var _xpsDocument = new XpsDocument(cTempDoc12Xps,
+            FileAccess.ReadWrite);
+
+            var xpsdw = XpsDocument.CreateXpsDocumentWriter(_xpsDocument);
+            xpsdw.Write(bDocumentPaginator);
+            _xpsDocument.Close();
+
+        }
+
+
+        // [WpfFact]
         public void Test2()
         {
-            var newMethod = NewMethod();
-            newMethod.Wait();
+            ManualResetEvent mevent = new ManualResetEvent(false);
+            var startSecondaryThread = RoslynCodeControl.StartSecondaryThread(mevent,(d) =>
+            {
+
+            });
+
+            mevent.ToTask().ContinueWith((B) =>
+            {
+                var d = Dispatcher.FromThread(startSecondaryThread);
+
+                var jtf2 = new JoinableTaskFactory(new JoinableTaskContext(RoslynCodeControl.SecondaryThread,
+                    new DispatcherSynchronizationContext(d)));
+                var jtf = new JoinableTaskFactory(new JoinableTaskContext());
+                jtf.Run(() => NewMethod(jtf, jtf2));
+            },CancellationToken.None,TaskContinuationOptions.None,TaskScheduler.Current).Wait();
+
         }
 
-        private static async Task NewMethod()
+        private static async Task<RoslynCodeBase> NewMethod(JoinableTaskFactory j, JoinableTaskFactory jtf2)
         {
-            RoslynCodeControl.StartSecondaryThread();
+            await j.SwitchToMainThreadAsync();
             RoslynCodeBase b = new RoslynCodeBase();
+
+            b.JTF = j;
+            b.JTF2 = jtf2;
             CodeIface c = b;
-            c.SourceText = "class T {}";
+            var code = File.ReadAllText(@"C:\temp\program.cs");
+            c.SourceText = code;
             var updateFormattedTextAsync = c.UpdateFormattedTextAsync();
             await updateFormattedTextAsync;
-            
-        }
-
-        [WpfFact]
-        public void Test1()
-        {
-            RoslynCodeControl.StartSecondaryThread();
-            RoslynCodeControl rcc = new RoslynCodeControl();
-            
-            TaskCompletionSource<bool> taskCompletion = new TaskCompletionSource<bool>();
-            rcc.AddHandler(RoslynCodeControl.RenderStartEvent, new RoutedEventHandler((sender, args) =>
-                {
-                    rcc.AddHandler(RoslynCodeControl.RenderCompleteEvent,
-                        new RoutedEventHandler((sender, args) =>
-                        {
-                            Debug.WriteLine("Render complete");
-
-                            taskCompletion.SetResult(true);
-                        }));
-                }
-            ));
-            rcc.Filename = @"C:\temp\program2.cs";
-            CommonText.UpdateFormattedText(rcc).Wait();
-
-            Assert.True(taskCompletion.Task.Wait(new TimeSpan(0, 0, 0, 5)));
+            return b;
         }
 
     }
